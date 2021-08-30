04c92a1d98497cc073c47c7e353a7097c5078f1f
arsen <asmandiyarov_ars@mail.ru>
2021-08-30 05:07:02 +0500

    HW3

--- simple_wsgi.py
+++
@@ -1,8 +0,0 @@
-def application(environ, start_response):
-    """
-    :param environ: словарь данных от сервера
-    :param start_response: функция для ответа серверу
-    """
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [b'Hello world from simple WSGI application!']
-
--- views.py
+++ views.py
@@ -2,15 +2,15 @@ from templator import render
 
 
 def index_view(request):
-    return '200 OK', render('index.html', object_list=[request]).encode('utf-8')
+    return '200 OK', render('index.html', object_list=[{'title': 'Главная', 'advert': 'здесь могла бы быть ваша реклама'}]).encode('utf-8')
 
 
 def about_view(request):
-    return '200 OK', render('about.html', object_list=[{'lesson': 1}]).encode('utf-8')
+    return '200 OK', render('about.html', object_list=[{'lesson': 3, 'title': 'О нас'}]).encode('utf-8')
 
 
 def contacts_view(request):
-    return '200 OK', render('contacts.html', object_list=[{'test': 'test'}]).encode('utf-8')
+    return '200 OK', render('contacts.html', object_list=[{'test': 'test', 'title': 'Контакты'}]).encode('utf-8')
 
 
 def not_found_view(request):
